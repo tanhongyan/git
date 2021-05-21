78e977e8b40f9165d8d9d8661c3d7c94df2a12c9
tanhongyan <2079076241@qq.com>
2021-05-21 11:49:30 +0800

    将student02提交远程仓库

--- src/com/git/TestGitDemo01.java
+++ src/com/git/TestGitDemo01.java
@@ -6,4 +6,7 @@ public class TestGitDemo01 {
         System.out.println("我修改了01");
         System.out.println("我再次修改了01hhhhhhhhhh");
     }
+    public void add(){
+        System.out.println("这是在student02分支上新增的方法");
+    }
 }
